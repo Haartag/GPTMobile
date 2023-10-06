7104a8a102caa4430df802b3ff865b57c4004c3c
Haartag <90470991+Haartag@users.noreply.github.com>
2023-10-07 00:00:50 +0200

    Resource wrapper

---
+++ app/src/main/java/com/llinsoft/gptmobile/utils/Resource.kt
@@ -0,0 +1,7 @@
+package com.llinsoft.gptmobile.utils
+
+sealed class Resource<out T> {
+    data class Success<out T>(val data: T) : Resource<T>()
+    //object Loading : Resource<Nothing>()
+    data class Error<out T>(val exception: Throwable) : Resource<T>()
+}
